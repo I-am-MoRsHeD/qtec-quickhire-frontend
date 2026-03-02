0674fa923a337f6a70051d6a919f18a3c65b121e
MoRsHeD <mdmorshed0187@gmail.com>
2026-03-02 15:06:29 +0600

    fixed hero image width issue

--- components/PublicPages/Home/Hero.tsx
+++ components/PublicPages/Home/Hero.tsx
@@ -3,8 +3,7 @@ import { Search, MapPin } from "lucide-react";
 
 const Hero = () => {
     return (
-        <section className="relative w-full min-h-187.5 bg-primary/5 overflow-hidden pt-32 pb-0">
-
+        <section className="relative w-full min-h-150 lg:min-h-187.5 bg-primary/5 overflow-hidden pt-32 pb-0">
             <div className="absolute inset-0 z-0 pointer-events-none">
                 <svg width="100%" height="100%" viewBox="0 0 1440 800" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1000 100L1300 250V550L1000 400V100Z" stroke="#4640DE" strokeOpacity="0.5" />
@@ -13,23 +12,19 @@ const Hero = () => {
                 </svg>
             </div>
 
-            <div className="absolute right-60 bottom-0 w-[50%] h-[90%] hidden lg:block z-10">
-                <Image
-                    src="/hero-person.png"
-                    alt="Job Seeker"
-                    fill
-                    className="object-contain object-bottom-right pointer-events-none"
-                    priority
-                />
-            </div>
+            <div className="max-w-310 mx-auto px-4 lg:px-0 relative z-30 h-full">
 
-            <div
-                className="absolute bottom-0 right-0 w-[45%] h-[35%] bg-background z-20 hidden lg:block"
-                style={{ clipPath: 'polygon(100% 0, 100% 100%, 0% 100%)' }}
-            />
+                <div className="absolute -right-10 bottom-0 w-[45%] h-[95%] hidden lg:block z-10">
+                    <Image
+                        src="/hero-person.png"
+                        alt="Job Seeker"
+                        fill
+                        className="object-contain object-bottom pointer-events-none"
+                        priority
+                    />
+                </div>
 
-            <div className="max-w-310 mx-auto px-4 lg:px-0 relative z-30">
-                <div className="lg:w-3/5 pt-10 pb-20">
+                <div className="lg:w-3/5 pt-10 pb-20 relative z-20">
                     <h1 className="font-clash text-[48px] md:text-[72px] font-bold leading-[1.05] text-foreground tracking-tight">
                         Discover <br /> more than <br />
                         <span className="text-chart-3 relative inline-block">
@@ -46,18 +41,18 @@ const Hero = () => {
                         Great platform for the job seeker that searching for new career heights and passionate about startups.
                     </p>
 
-                    <div className="mt-10 bg-background p-4 shadow-[0px_24px_48px_rgba(0,0,0,0.04)] flex flex-col md:flex-row items-center gap-4 border border-[#D6DDEB] relative lg:-mr-20 z-30">
-                        <div className="flex items-center gap-3 flex-1 w-full md:border-r border-[#D6DDEB] px-2">
-                            <Search className="text-[#252733]" size={24} />
+                    <div className="mt-10 bg-background p-4 shadow-[0px_24px_48px_rgba(0,0,0,0.04)] flex flex-col md:flex-row items-center gap-4 border border-border relative lg:-mr-25 z-30">
+                        <div className="flex items-center gap-3 flex-1 w-full md:border-r border-border px-2">
+                            <Search className="text-foreground" size={24} />
                             <input
                                 type="text"
                                 placeholder="Job title or keyword"
-                                className="font-epilogue w-full outline-none text-[#252733] py-2 bg-transparent"
+                                className="font-epilogue w-full outline-none text-foreground py-2 bg-transparent"
                             />
                         </div>
                         <div className="flex items-center gap-3 flex-1 w-full px-2">
-                            <MapPin className="text-[#252733]" size={24} />
-                            <select className="font-epilogue w-full outline-none text-[#252733] bg-transparent appearance-none cursor-pointer">
+                            <MapPin className="text-foreground" size={24} />
+                            <select className="font-epilogue w-full outline-none text-foreground bg-transparent appearance-none cursor-pointer">
                                 <option>Florence, Italy</option>
                             </select>
                         </div>
@@ -67,10 +62,15 @@ const Hero = () => {
                     </div>
 
                     <p className="mt-6 text-foreground/70 font-epilogue text-sm">
-                        <span className="">Popular :</span> UI Designer, UX Researcher, Android, Admin
+                        Popular : UI Designer, UX Researcher, Android, Admin
                     </p>
                 </div>
             </div>
+
+            <div
+                className="absolute bottom-0 right-0 w-[40%] h-[30%] bg-background z-999 hidden lg:block"
+                style={{ clipPath: 'polygon(100% 0, 100% 100%, 0% 100%)' }}
+            />
         </section>
     );
 };
